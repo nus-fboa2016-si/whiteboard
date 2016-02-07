0008a83cb0a3a1d3d133172a4458e8b8e9c8c2ec
Yijin <leowyijin@gmail.com>
2016-02-07 17:09:55 +0800

    track connection count and client lines on server

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var numConnected = 0;
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('connected: ' + ++numConnected);
+    socket.on('disconnect', function() {
+        console.log('connected: ' + --numConnected);
+    });
+    socket.on('draw line', function(x0, y0, x1, y1) {
+        //console.log(x0 + "," + y0 + " " + x1 + "," + y1);
+    });
 });
 
 http.listen(3000, function() {
