4c0c18b361e03b27270d4a783e680e2605f84eaa
Yijin <leowyijin@gmail.com>
2016-03-08 16:40:43 +0800

    updated v1, cleaned v2-4

--- guide/v2/public/whiteboard.js
+++ guide/v2/public/whiteboard.js
@@ -1,3 +1,36 @@
-/**
- * Created by Prime Mover on 4/3/2016.
- */
+// size the whiteboard to parent container
+var wb = document.getElementById('whiteboard');
+var container = document.getElementById('whiteboard-container');
+wb.height = container.clientHeight;
+wb.width = container.clientWidth;
+
+// set stroke style
+var ctx = wb.getContext('2d');
+ctx.strokeStyle = '#ffffff';
+ctx.lineWidth = 2;
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
+
+var isDrawing = false;
+var prevX = null;
+var prevY = null;
+
+// event handlers
+wb.onmousedown = function(e){
+  isDrawing = true;
+  prevX = e.offsetX;
+  prevY = e.offsetY;
+};
+wb.onmousemove = function(e){
+  if (!isDrawing) return;
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  prevX = e.offsetX;
+  prevY = e.offsetY;
+};
+wb.onmouseup = function(e){
+  isDrawing = false;
+  prevX = prevY = null;
+};
--- guide/v3/public/whiteboard.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Prime Mover on 4/3/2016.
- */
