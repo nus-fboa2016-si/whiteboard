c260d5876c4ef64560844b2fdedc1e462d8cdc76
Yijin <leowyijin@gmail.com>
2016-03-08 16:28:14 +0800

    slight additions to v1

--- guide/v1/index.js
+++ guide/v1/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root: __dirname});
+});
+
 http.listen(3000, function(){
   console.log('listening on port 3000');
 });
