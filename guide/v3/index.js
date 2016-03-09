847204b5a7b53de35199ceb350699c256fe5fbe1
Yijin <leowyijin@gmail.com>
2016-03-09 23:37:36 +0800

    v3 files

---
+++ guide/v3/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var userCount = 0;
+
+//app.get('/', function(req, res){
+//  res.sendFile('index.html', {root: __dirname});
+//});
+
+app.use(express.static('public'));
+
+io.on('connection', function(socket){
+
+  // update connection count
+  userCount++;
+  console.log('Users connected: ' + userCount);
+  io.emit('user count', userCount); //update all clients
+
+  socket.on('disconnect', function(){
+    userCount--;
+    console.log('Users connected: ' + userCount);
+    io.emit('user count', userCount);
+  });
+
+  socket.on('draw line', function(line){
+    socket.broadcast.emit('draw line', line);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
---
+++ guide/v3/public/whiteboard.js
@@ -0,0 +1,71 @@
+var socket = io();
+
+// size the whiteboard to parent container
+var container = document.getElementById('whiteboard-container');
+var wb = container.querySelector('#whiteboard');
+wb.height = container.clientHeight;
+wb.width = container.clientWidth;
+
+var isDrawing = false;
+var prevX = null;
+var prevY = null;
+var colorString = '#aa88ff';
+
+// set stroke style
+var ctx = wb.getContext('2d');
+ctx.lineWidth = 2;
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
+
+// event handlers
+container.onmousedown = function(e){
+  e.preventDefault();
+  isDrawing = true;
+  var x = e.offsetX + e.target.getBoundingClientRect().left - container.getBoundingClientRect().left;
+  var y = e.offsetY + e.target.getBoundingClientRect().top - container.getBoundingClientRect().top;
+  prevX = x;
+  prevY = y;
+};
+
+container.onmousemove = function(e){
+  if (!isDrawing) return;
+  var x = e.offsetX + e.target.getBoundingClientRect().left - container.getBoundingClientRect().left;
+  var y = e.offsetY + e.target.getBoundingClientRect().top - container.getBoundingClientRect().top;
+  var newLine = {
+    startX: prevX,
+    startY: prevY,
+    endX: x,
+    endY: y,
+    colorString: colorString
+  };
+  drawLine(newLine);
+  socket.emit('draw line', newLine);
+  prevX = x;
+  prevY = y;
+};
+
+container.onmouseup = function(e){
+  isDrawing = false;
+};
+
+socket.on('draw line', drawLine);
+function drawLine(line) {
+  ctx.strokeStyle = line.colorString;
+  ctx.beginPath();
+  ctx.moveTo(line.startX, line.startY);
+  ctx.lineTo(line.endX, line.endY);
+  ctx.stroke();
+}
+
+socket.on('user count', updateUserCount);
+var userCountText = container.querySelector('text.user-count');
+function updateUserCount(count) {
+  userCountText.textContent = 'CONNECTED: ' + count;
+}
+
+var colorPickerWrapper = container.querySelector('.color-picker-wrapper');
+var colorPickerInput = colorPickerWrapper.querySelector('input.color-picker');
+colorPickerInput.onchange = function() {
+  colorPickerWrapper.style.backgroundColor = colorPickerInput.value;
+  colorString = colorPickerInput.value;
+};
