5f1b5d12d50ef52c73e0f1237642b687aba2e828
Leow Yijin <leowyijin@gmail.com>
2016-03-30 23:38:53 +0800

    updated wb.js v4 snapshot

--- guide/v4/public/whiteboard.js
+++ guide/v4/public/whiteboard.js
@@ -90,7 +90,6 @@ colorPickerInput.onchange = function() {
 
 // particle effects ------------------------------------
 
-var unanimatedLines = [];
 var tick = 0, clock = new THREE.Clock(true);
 var gfxCanvas = container.querySelector('canvas.gfx-layer');
 
@@ -107,12 +106,16 @@ var camera = new THREE.PerspectiveCamera(
     }),
     scene = new THREE.Scene();
 
+camera.position.z = 100;
+renderer.setSize(container.offsetWidth, container.offsetHeight);
+renderer.setClearColor(0x000000, 0);
+
 var particleSystem = new THREE.GPUParticleSystem({maxParticles: 250000}),
-    spawnerOpts = {
+var spawnerOpts = {
       spawnRate: 3000,
       timeScale: 1
-},
-    particleOpts = {
+};
+var particleOpts = {
       positionRandomness: 0.5,
       velocity: new THREE.Vector3(),
       velocityRandomness: 0.5,
@@ -123,9 +126,6 @@ var particleSystem = new THREE.GPUParticleSystem({maxParticles: 250000}),
       sizeRandomness: 1
 };
 
-camera.position.z = 100;
-renderer.setSize(container.offsetWidth, container.offsetHeight);
-renderer.setClearColor(0x000000, 0);
 scene.add(particleSystem);
 
 animate();
@@ -152,11 +152,9 @@ function spawnParticlesAlongLine(number, line) {
         line.startX * (1 - percent) + line.endX * percent,
         line.startY * (1 - percent) + line.endY * percent
     );
-    // convert our colorString into RGB hex for the particle system
-    particleOpts.color = parseInt(line.colorString.substr(1), 16);
-    // start position of this particle
-    particleOpts.position = new THREE.Vector3(pos.x, pos.y, 0);
-    particleSystem.spawnParticle(particleOpts); // Let it gooo..
+    particleOpts.color = parseInt(line.colorString.substr(1), 16); // convert our colorString into RGB hex for the particle system
+    particleOpts.position = new THREE.Vector3(pos.x, pos.y, 0); // start position of this particle
+    particleSystem.spawnParticle(particleOpts);
   }
 }
 
